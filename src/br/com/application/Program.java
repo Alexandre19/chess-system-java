05ce249c7d7d2447d329d1b8720b9d36ca1f7d81
Alexandre Borges <alexandreborges.goncalves@gmail.com>
2022-09-02 12:30:32 -0300

    chess layer and printing the board

--- src/br/com/application/Program.java
+++ src/br/com/application/Program.java
@@ -1,11 +1,16 @@
 package br.com.application;
 
+import br.com.chess.ChessMatch;
+
+
 public class Program {
 
 	public static void main(String[] args) {
 	
-		System.out.println("HEllo World"); 
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
-
+	
+	
 }
